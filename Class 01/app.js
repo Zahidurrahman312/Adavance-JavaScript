372828e812fbf133936fc051726f573662ba8e2c
unknown <zahidrehamn886554@gmail.com>
2025-12-02 03:01:43 +0500

    object

---
+++ Class 01/app.js
@@ -0,0 +1,67 @@
+console.log("JavaScript connected!");
+
+// let obj = {
+//     name:"zahid",
+//     age:24,
+//     email:"zahid@gmail.com",
+// };
+
+// console.log(obj);
+// //specific value target with two method
+
+// console.log(obj.name);
+// console.log(obj["email"]);
+
+// console.log("name" in obj);
+// console.log("email" in obj);
+
+// obj.month = "July";
+
+// console.log(obj);
+
+// delete obj.month;
+
+// console.log(obj);
+
+// function Student(name,age,city){
+//     this.name=name;
+//     this.age=age;
+//     this.city=city;
+// };
+
+// let student1= new Student("zahid",24,"karachi");
+
+// console.log(student1);
+
+// Student.prototype.sayHi = function(){
+//     console.log("Hi, my name is " + this.name);
+    
+// }
+
+// let sutend2 = new Student("Ahmd",22,"lahore");
+
+// sutend2.sayHi();
+
+// Student.prototype.canellable = true;
+
+// console.log(sutend2.canellable);
+// console.log(student1.canellable);
+// student1.canellable = false;
+// console.log(student1.hasOwnProperty("cancellable"));
+// console.log(sutend2.hasOwnProperty("cancellable"));
+
+// let obj ={
+//     name:"zahid",
+//     age:24,
+//     email:"zahid@gmail.com",
+// };
+
+// var listProprty = [];
+
+// for(var porp in obj){
+//     // listProprty.push(obj[porp]);
+//     listProprty.push({ key: porp,value: obj[porp]})
+// }
+// console.log(listProprty);
+
+
